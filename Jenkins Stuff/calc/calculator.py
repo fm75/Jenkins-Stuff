88c31e8ab3359b822aea19996106eebc748cdb04
fred <FMitchell75@gmail.com>
2013-08-03 17:00:26 -0400

    First cut

---
+++ Jenkins Stuff/calc/calculator.py
@@ -0,0 +1,16 @@
+class Calculator(object):
+    
+    
+    def add(self, a, b):
+      return a + b
+
+    
+    def sub(self, b, a):
+      return b - a
+    
+    
+    
+    
+
+
+
---
+++ Jenkins Stuff/calc/text/TestCalc.py
@@ -0,0 +1,21 @@
+'''
+Created on Aug 3, 2013
+
+@author: fred
+'''
+import unittest
+from calc.calculator import Calculator
+
+class Test(unittest.TestCase):
+
+  def testAdd(self):
+    calculator = Calculator ()
+    self.assertEqual(4, calculator.add (a=1, b=3))
+
+  def testSub (self):
+    calculator = Calculator ()
+    self.assertEqual(2, calculator.sub(b=3,a=1))
+     
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()
